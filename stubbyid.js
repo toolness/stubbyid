4eb419562d582e15835dbfb7a4afdf9ebd18a335
Atul Varma <varmaa@gmail.com>
2013-05-02 16:19:58 -0400

    Origination.

---
+++ stubbyid.js
@@ -0,0 +1,190 @@
+(function() {
+  "use strict";
+
+  var LOGIN_STATE_KEY = "STUBBYID_LOGIN_STATE";
+  var widget = {
+    el: document.createElement('div'),
+    update: function() {
+      var state = getLoginState();
+      if (state) {
+        widget.el.innerHTML = 'Persona thinks user wants to be logged in ' +
+                              'as <strong>' + escapeHtml(state) +
+                              '</strong>. <button>logout</button>';
+      } else {
+        widget.el.innerHTML = 'Persona thinks user wants to be logged out. ' +
+                              '<button>login</button>';
+      }
+    },
+    init: function() {
+      widget.el.style.position = "absolute";
+      widget.el.style.bottom = "0";
+      widget.el.style.right = "0";
+      widget.el.style.backgroundColor = "lightgray";
+      widget.el.style.padding = "4px";
+      widget.el.style.zIndex = "100000";
+      attach(widget.el, "click", function(event) {
+        if (target(event).nodeName == "BUTTON") {
+          if (getLoginState())
+            setLoginState(null);
+          else
+            setLoginState(window.prompt("Enter email address") || null);
+        }
+      });
+      document.body.appendChild(widget.el);
+      widget.update();
+    }
+  };
+  var escapeHtml = function(string) {
+    var entityMap = {
+       "&": "&amp;",
+       "<": "&lt;",
+       ">": "&gt;",
+       '"': '&quot;',
+       "'": '&#39;',
+       "/": '&#x2F;'
+     };
+
+     return String(string).replace(/[&<>"'\/]/g, function (s) {
+       return entityMap[s];
+     });
+  };
+  var target = function(event) {
+    return event.target || event.srcElement;
+  };
+  var attach = function(element, eventName, cb) {
+    if (element.addEventListener)
+      element.addEventListener(eventName, cb, false);
+    else
+      element.attachEvent('on' + eventName, cb);
+  };
+  var setLoginState = function(state) {
+    state = state || null;
+    if (getLoginState() === state)
+      return;
+    if (state) {
+      window.localStorage.setItem(LOGIN_STATE_KEY, state);
+      widget.update();
+      watchOptions.onlogin(state);
+    } else {
+      window.localStorage.removeItem(LOGIN_STATE_KEY);
+      widget.update();
+      watchOptions.onlogout();
+    }
+  };
+  var getLoginState = function() {
+    return window.localStorage.getItem(LOGIN_STATE_KEY) || null;
+  };
+  var fail = function(msg) {
+    log(msg);
+    throw new Error(msg);
+  };
+  var log = function(msg) {
+    if (window.console && window.console.log)
+      window.console.log("STUBBYID: " + msg);
+  };
+  var watchOptions = {
+    _onlogin: null,
+    _onlogout: null,
+    onlogin: function(assertion) {
+      if (watchOptions._onlogin) {
+        log("Calling onlogin().");
+        watchOptions._onlogin(assertion);
+      }
+    },
+    onlogout: function() {
+      if (watchOptions._onlogout) {
+        log("Calling onlogout().");
+        watchOptions._onlogout();
+      }
+    }
+  };
+
+  window.navigator.id = {
+    stubby: {
+      reset: function() {
+        watchOptions._onlogin = null;
+        watchOptions._onlogout = null;
+        setLoginState(null);
+      },
+      setPersonaState: setLoginState,
+      getPersonaState: getLoginState,
+      widgetElement: widget.el
+    },
+    // https://developer.mozilla.org/en-US/docs/DOM/navigator.id.watch
+    watch: function navigator_id_watch(options) {
+      if (!(typeof(options.loggedInUser) == "undefined" ||
+            typeof(options.loggedInUser) == "string" ||
+            options.loggedInUser === null))
+        fail("loggedInUser must be null, undefined, or string");
+      if (typeof(options.onlogin) != "function")
+        fail("onlogin must be a function");
+      if (typeof(options.onlogout) != "function")
+        fail("onlogout must be a function");
+
+      var personaState = getLoginState();
+      var loggedInUser = options.loggedInUser;
+      var reasoning = "";
+
+      watchOptions._onlogin = options.onlogin;
+      watchOptions._onlogout = options.onlogout;
+
+      if (typeof(loggedInUser) == "undefined") {
+        reasoning = "Client doesn't know if user is logged in or not ";
+        if (personaState) {
+          log(reasoning + "and they want to be logged in as " +
+              personaState + ".");
+          watchOptions.onlogin(personaState);
+        } else {
+          log(reasoning + "and they want to be logged out. ");
+          watchOptions.onlogout();
+        }
+      } else if (typeof(loggedInUser) == "string") {
+        reasoning = "Client thinks the user is logged in as " +
+                    loggedInUser + " ";
+
+        if (personaState) {
+          if (personaState == loggedInUser) {
+            log(reasoning + "and they want to be, so doing nothing.");
+          } else {
+            log(reasoning + "but they want to be logged in as " +
+                personaState + ".");
+            watchOptions.onlogin(personaState);
+          }
+        } else {
+          log(reasoning + "but they want to be logged out.");
+          watchOptions.onlogout();
+        }
+      } else if (loggedInUser === null) {
+        reasoning = "Client thinks the user is logged out ";
+        if (personaState) {
+          log(reasoning + "but they want to be logged in as " +
+              personaState + ".");
+          watchOptions.onlogin(personaState);
+        } else {
+          log(reasoning + "and they want to be, so doing nothing.");
+        }
+      }
+    },
+    request: function navigator_id_request(options) {
+      options = options || {};
+      var response = window.prompt("Enter email address");
+      if (!response) {
+        if (options.oncancel)
+          options.oncancel();
+        return;
+      }
+      setLoginState(response);
+    },
+    logout: function navigator_id_logout() {
+      setLoginState(null);
+    },
+    get: function navigator_id_get() {
+      fail("only the Observer API is supported");
+    }
+  };
+
+  if (document.readyState == "complete")
+    widget.init();
+  else
+    attach(window, "load", widget.init);
+})();
---
+++ test/tests.js
@@ -0,0 +1,225 @@
+onload = function() {
+  var log = [];
+  var stubby = navigator.id.stubby;
+  var widget = stubby.widgetElement;
+
+  // IE8 Fixes.
+  if (!window.console) window.console = {};
+  if (!window.console.log) window.console.log = function() {};
+  if (typeof(window.prompt) != "function") window.prompt = function() {};
+
+  widget.style.display = "none";
+  module("stubbyid", {
+    setup: function() {
+      stubby.reset();
+      log = [];
+      sinon.stub(window.console, "log", function(msg) {
+        log.push(msg);
+      });
+    },
+    teardown: function() {
+      window.console.log.restore();
+    }
+  });
+
+  test("loggedInUser=null, personaState=null", function() {
+    var onlogin = sinon.spy();
+    var onlogout = sinon.spy();
+    stubby.setPersonaState(null);
+    navigator.id.watch({
+      loggedInUser: null,
+      onlogin: onlogin,
+      onlogout: onlogout
+    });
+    equal(onlogin.callCount, 0);
+    equal(onlogout.callCount, 0);
+    deepEqual(log, ["STUBBYID: Client thinks the user is logged out " +
+                    "and they want to be, so doing nothing."]);
+  });
+
+  test("loggedInUser=foo@example, personaState=foo@example", function() {
+    var onlogin = sinon.spy();
+    var onlogout = sinon.spy();
+    stubby.setPersonaState("foo@example.com");
+    navigator.id.watch({
+      loggedInUser: "foo@example.com",
+      onlogin: onlogin,
+      onlogout: onlogout
+    });
+    equal(onlogin.callCount, 0);
+    equal(onlogout.callCount, 0);
+    deepEqual(log, ["STUBBYID: Client thinks the user is logged in as " +
+                    "foo@example.com and they want to be, so doing " +
+                    "nothing."]);
+  });
+
+  test("loggedInUser=foo@example, personaState=null", function() {
+    var onlogin = sinon.spy();
+    var onlogout = sinon.spy();
+    stubby.setPersonaState(null);
+    navigator.id.watch({
+      loggedInUser: "foo@example.com",
+      onlogin: onlogin,
+      onlogout: onlogout
+    });
+    equal(onlogin.callCount, 0);
+    equal(onlogout.callCount, 1);
+    deepEqual(log, ["STUBBYID: Client thinks the user is logged in as " +
+                    "foo@example.com but they want to be logged out.",
+                    "STUBBYID: Calling onlogout()."]);
+  });
+
+  test("loggedInUser=undefined, personaState=null", function() {
+    var onlogin = sinon.spy();
+    var onlogout = sinon.spy();
+    stubby.setPersonaState(null);
+    navigator.id.watch({
+      loggedInUser: undefined,
+      onlogin: onlogin,
+      onlogout: onlogout
+    });
+    equal(onlogin.callCount, 0);
+    equal(onlogout.callCount, 1);
+    deepEqual(log, ["STUBBYID: Client doesn't know if user is logged in " +
+                    "or not and they want to be logged out. ",
+                    "STUBBYID: Calling onlogout()."]);
+  });
+
+  test("loggedInUser=null, personaState=foo@example", function() {
+    var onlogin = sinon.spy();
+    var onlogout = sinon.spy();
+    stubby.setPersonaState("foo@example.com");
+    navigator.id.watch({
+      loggedInUser: null,
+      onlogin: onlogin,
+      onlogout: onlogout
+    });
+    equal(onlogin.callCount, 1);
+    ok(onlogin.calledWith("foo@example.com"));
+    equal(onlogout.callCount, 0);
+    deepEqual(log, ["STUBBYID: Client thinks the user is logged out but " +
+                    "they want to be logged in as foo@example.com.",
+                    "STUBBYID: Calling onlogin()."]);
+  });
+
+  test("loggedInUser=undefined, personaState=foo@example", function() {
+    var onlogin = sinon.spy();
+    var onlogout = sinon.spy();
+    stubby.setPersonaState("foo@example.com");
+    navigator.id.watch({
+      loggedInUser: undefined,
+      onlogin: onlogin,
+      onlogout: onlogout
+    });
+    equal(onlogin.callCount, 1);
+    ok(onlogin.calledWith("foo@example.com"));
+    equal(onlogout.callCount, 0);
+    deepEqual(log, ["STUBBYID: Client doesn't know if user is logged in " +
+                    "or not and they want to be logged in as " +
+                    "foo@example.com.",
+                    "STUBBYID: Calling onlogin()."]);
+  });
+
+  test("loggedInUser=bar@example, personaState=foo@example", function() {
+    var onlogin = sinon.spy();
+    var onlogout = sinon.spy();
+    stubby.setPersonaState("foo@example.com");
+    navigator.id.watch({
+      loggedInUser: "bar@example.com",
+      onlogin: onlogin,
+      onlogout: onlogout
+    });
+    equal(onlogin.callCount, 1);
+    ok(onlogin.calledWith("foo@example.com"));
+    equal(onlogout.callCount, 0);
+    deepEqual(log, ["STUBBYID: Client thinks the user is logged in as " +
+                    "bar@example.com but they want to be logged in as " +
+                    "foo@example.com.",
+                    "STUBBYID: Calling onlogin()."]);
+  });
+
+  test("widget works when persona state is logged in", function() {
+    stubby.setPersonaState("b<script>alert('e');</script>'");
+    ok(widget.innerHTML.indexOf("b&lt;script&gt;alert('e')") != -1);
+    ok(widget.innerHTML.indexOf("logout") != -1);
+    widget.querySelector("button").click();
+    ok(widget.innerHTML.indexOf("logout") == -1);
+    equal(stubby.getPersonaState(), null);
+  });
+
+  test("widget works when persona state is logged out", function() {
+    stubby.setPersonaState(null);
+    ok(widget.innerHTML.indexOf("login") != -1);
+    sinon.stub(window, "prompt", function(prompt) {
+      return "lol@cat.org";
+    });
+    widget.querySelector("button").click();
+    ok(window.prompt.calledOnce);
+    window.prompt.restore();
+    ok(widget.innerHTML.indexOf("login") == -1);
+    equal(stubby.getPersonaState(), "lol@cat.org");
+  });
+
+  test("navigator.id.request() calls oncancel if needed", function() {
+    var opts = {oncancel: sinon.spy()};
+    var promptRes;
+    stubby.setPersonaState(null);
+    navigator.id.watch({
+      loggedInUser: null,
+      onlogin: function() {},
+      onlogout: function() {}
+    });
+    sinon.stub(window, "prompt", function(prompt) { return promptRes; });
+    promptRes = "foo@example.com";
+    navigator.id.request(opts);
+    equal(opts.oncancel.callCount, 0);
+    promptRes = null;
+    navigator.id.request(opts);
+    ok(opts.oncancel.calledOnce);
+    window.prompt.restore();
+  });
+
+  test("navigator.id.request() works", function() {
+    var onlogin = sinon.spy();
+    var onlogout = sinon.spy();
+    stubby.setPersonaState(null);
+    navigator.id.watch({
+      loggedInUser: null,
+      onlogin: onlogin,
+      onlogout: onlogout
+    });
+    sinon.stub(window, "prompt", function(prompt) {
+      return "lol@cat.org";
+    });
+    navigator.id.request();
+    ok(window.prompt.calledOnce);
+    window.prompt.restore();
+    equal(onlogin.callCount, 1);
+    ok(onlogin.calledWith("lol@cat.org"));
+    equal(onlogout.callCount, 0);
+    ok(widget.innerHTML.indexOf("login") == -1);
+    equal(stubby.getPersonaState(), "lol@cat.org");    
+  });
+
+  test("navigator.id.logout() works", function() {
+    var onlogin = sinon.spy();
+    var onlogout = sinon.spy();
+    stubby.setPersonaState("lol@cat.org");
+    navigator.id.watch({
+      loggedInUser: "lol@cat.org",
+      onlogin: onlogin,
+      onlogout: onlogout
+    });
+    navigator.id.logout();
+    equal(onlogin.callCount, 0);
+    equal(onlogout.callCount, 1);
+    ok(widget.innerHTML.indexOf("login") != -1);
+    equal(stubby.getPersonaState(), null);
+  });
+
+  test("navigator.id.get() throws unimplemented error", function() {
+    throws(function() {
+      navigator.id.get();
+    }, /only the Observer API is supported/);
+  });
+};
